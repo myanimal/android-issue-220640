4ad09a8f2b34c5e18421b688fe24b00847bb6a4a
Alex Koller <myanimal@gmail.com>
2016-09-06 15:32:31 +0200

    Working example.

---
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void useMainActivity() throws Exception {
+        new MainActivity();
+    }
+}
